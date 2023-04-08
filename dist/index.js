050a39b125688f55174eddf85a47919c26b64bde
Chuckie <devchuckcamp@gmail.com>
2023-04-07 18:35:08 -0700

    cors list update

--- dist/index.js
+++ dist/index.js
@@ -2,21 +2,21 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a, _b;
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = require("mongoose");
 const todoRoutes_1 = __importDefault(require("./routes/todoRoutes"));
-const allowedOrigins = (_a = process.env.front_end_domain) !== null && _a !== void 0 ? _a : '*';
+const allowedOrigins = process.env.front_end_domain ? [process.env.front_end_domain] : '*';
 const options = {
     origin: allowedOrigins
 };
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.port || 3001;
-const mongoConnectionString = (_b = process.env.mongodburi) === null || _b === void 0 ? void 0 : _b.toString();
+const mongoConnectionString = (_a = process.env.mongodburi) === null || _a === void 0 ? void 0 : _a.toString();
 const apiVersion = process.env.api_version || "V1";
 // Middleware
 app.use((0, cors_1.default)(options));
